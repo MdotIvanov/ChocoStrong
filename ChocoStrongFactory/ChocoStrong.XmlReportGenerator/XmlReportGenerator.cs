67802171c7a20d58f0d20537c1dd2f73e2fe1f70
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-08-30 17:30:02 +0300

    More Code added to the XMLWriter class

--- ChocoStrongFactory/ChocoStrong.XmlReportGenerator/XmlReportGenerator.cs
+++ ChocoStrongFactory/ChocoStrong.XmlReportGenerator/XmlReportGenerator.cs
@@ -9,19 +9,21 @@ namespace ChocoStrong.XmlReportGenerator
 {
     public class XmlReportGenerator
     {
-
-
-
         public bool GenerateReport()
         {
             bool isReportCreared = false;
 
-            var writer = new XmlTextWriter(table.xml);
+            string fileName = "../../table.xml";
+            Encoding encoding = Encoding.GetEncoding("windows-1251");
+            var writer = new XmlTextWriter(fileName, encoding);
             
             using (writer)
             {
+                writer.Formatting = Formatting.Indented;
+                writer.IndentChar = '\t';
+                writer.Indentation = 1;
 
-
+                writer.WriteStartDocument();
 
 
 
