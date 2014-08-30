d505fc704319cbae27cfc34b22d5550cd98ef7c6
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-08-30 20:24:18 +0300

    XmlReader class creation

---
+++ ChocoStrongFactory/ChocoStrong.XMLImporter/XmlImporter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ChocoStrong.XmlImporter
+{
+    public class XmlImporter
+    {
+        public void ReadReport()
+        {
+            string fileName = "../../xmlFile.xml";
+            var reader = XmlReader.Create(fileName);
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) &&
+                        (reader.Name == "Shop Name"))
+                    {
+                        Console.WriteLine(reader.ReadElementString());
+                    }
+                }
+            }
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    if (reader.NodeType == XmlNodeType.Element)
+                    {
+                        Console.WriteLine(reader.Name);
+                    }
+                }
+            }
+
+        }
+    }
+}
