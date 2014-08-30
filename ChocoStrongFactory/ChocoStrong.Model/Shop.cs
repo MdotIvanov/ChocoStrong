42115f673ea752246f7642f9f34e61d5eb820b5c
xenry <xenryy@gmail.com>
2014-08-30 14:40:50 +0300

    adding missing files

---
+++ ChocoStrongFactory/ChocoStrong.Model/Shop.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocoStrong.Model
+{
+    public class Shop
+    {
+        private ICollection<Sale> mSales;
+
+        public Shop()
+        {
+            mSales = new HashSet<Sale>();
+        }
+
+        public int ShopId { get; set; }
+
+        public string ShopName { get; set; }
+
+        public virtual ICollection<Sale> Sales
+        {
+            get { return mSales; }
+            set { mSales = value; }
+        }
+
+        public int LocationId { get; set; }
+
+        public virtual City Location { get; set; }
+    }
+}
