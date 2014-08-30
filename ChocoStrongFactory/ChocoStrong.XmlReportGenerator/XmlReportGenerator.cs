ec48e142f2ebad9173d93554d4cfbeed75cb5d62
hristofornikolov <hristofor.nikolov@yahoo.co.uk>
2014-08-30 14:58:49 +0300

    XmlReportGenerator class creation

---
+++ ChocoStrongFactory/ChocoStrong.XmlReportGenerator/XmlReportGenerator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocoStrong.XmlReportGenerator
+{
+    public class XmlReportGenerator
+    {
+
+
+
+        public bool GenerateReport()
+        {
+            bool isReportCreared = false;
+
+
+
+            return isReportCreared;
+        }
+    }
+}
